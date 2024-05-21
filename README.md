d974d9c7db43640072cbc60f93cde4081d986544
Mieszko Gulinski <mieszko_gulinski@radcode.co>
2024-05-21 21:06:01 +0200

    add more test cases

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ To run: `node index.js`
 
 ## Attitude determination algorithm
 
-The algorithm for adjusting the satellite's orientation will be as follows:
+The algorithm for determining the satellite's orientation will be as follows:
 
 1. the thermal camera takes a picture
 2. the image undergoes thresholding, to distinguish between the Earth's surface and the sky
@@ -18,9 +18,32 @@ The algorithm for adjusting the satellite's orientation will be as follows:
 5. the image is divided into 4 quadrants, and the number of invalid pixels in each quadrant is calculated; let's call them UL, UR, LL, LR (upper left, upper right, lower left, lower right)
 6. (UL+UR)-(LL+LR) is calculated. If it's zero, the satellite is oriented correctly in the axis parallel to Earth and perpendicular to the direction of camera view. If it's positive, the satellite is oriented too much towards the Earth's surface, and if it's negative, the satellite is oriented too much towards the sky, as viewed by the camera.
 7. (UL+LR)-(UR+LL) is calculated. If it's zero, the satellite is oriented correctly in the axis parallel to Earth going towards the direction of camera view. If it's positive, the satellite is tilted counterclockwise, and if it's negative, the satellite is tilted too much clockwise from the camera viewpoint.
-8. using the magnetorques, the satellite's orientation is adjusted so that both of the above values are zero.
 
-The algorithm can be performed on images from several sides of the satellite, to eliminate images from the direction of the Sun, and to provide redundancy in case one of the cameras fails. Direction towards the Sun can be determined using photodiode-based Sun sensors, or by measuring output voltage of the solar panels on each side of the satellite.
+Then, the orientation of the satellite determined by the algorithm can be used to adjust the satellite's orientation using reaction wheels, magnetic torquers, or other means.
+
+### Convention of the axes and satellite sides
+
+TODO: add a diagram of the satellite, axes and sides, and the camera placement
+
+The satellite is assumed to be cube-shaped. The axes are named X, Y, Z. When the satellite is properly oriented, the Z axis is intended to point towards the zenith.
+
+The satellite sides are named X+, X-, Y+, Y-, Z+, Z-. The Z+ side is intended to point towards the zenith, and the Z- side is intended to point towards nadir (towards the Earth center). Thermal cameras are placed on the X+, Y+, X- and Y- sides of the satellite, and should see both the Earth's surface and the sky. It's unimportant how the satellite is rotated in the Z axis, as long as the Z+ side is pointing towards the zenith.
+
+If a camera mounted on the X+ side sees imbalance in the (UL+UR)-(LL+LR) value, it means that the satellite needs to be rotated in the Y axis. At the same time, the camera mounted on the X- side should see the same imbalance, but with the opposite sign, and the cameras mounted on the Y+ and Y- sides should see imbalance in the (UL+LR)-(UR+LL) value.
+
+Similarly, if a camera mounted on the X+ side sees imbalance in the (UL+LR)-(UR+LL) value, it means that the satellite needs to be rotated in the X axis. At the same time, the camera mounted on the X- side should see the same imbalance, but with the opposite sign, and the cameras mounted on the Y+ and Y- sides should see imbalance in the (UL+UR)-(LL+LR) value.
+
+If the camera is mounted on the X+ or X- side, the axis perpendicular to the camera view is the Y axis, and the axis parallel to the camera view is the X axis.
+
+### Multiple cameras
+
+This way, while in principle only one of the cameras is needed to determine and adjust the satellite's orientation, multiple cameras can be used to improve accuracy of the algorithm and robustness against sunlight, noise and cloud cover, and provide redundancy in case of a camera failure.
+
+Direction towards the Sun can be determined using photodiode-based Sun sensors, or by measuring output voltage of the solar panels on each side of the satellite. The view from a camera mounted on a side of the satellite facing the Sun should be ignored, as the sunlight would cause the image to be saturated.
+
+### Assumption about Earth visibility
+
+It is assumed that the limb of the Earth is a straight line. This simplification works when the satellite is on a low Earth orbit, and the camera uses rectilinear projection (it's not a fisheye lens). A satellite on a higher orbit will see Earth as a circle, and a different algorithm will be necessary in such case.
 
 ## Simulation
 
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const calculateBalance = require("./calculateBalance");
+const templates = require("./templates");
+const inputModifiers = require("./inputModifiers");
 
 const width = 16;
 const height = 12;
@@ -9,186 +11,52 @@ const temperatureThreshold = -20; // threshold for the temperature, in Celsius, 
 // In the simulation, the range of the visible temperature is from -128 to 127 degrees Celsius.
 // In reality, the temperature will be clamped to the range of the sensor, roughly -40 to +300 degrees Celsius.
 
-// Case 1: The satellite is correctly oriented - the upper half of the image is the sky, the lower half is the Earth.
-// The temperature of the sky and the Earth is uniform. Should return that the satellite is correctly oriented and does not need to be rotated.
+// Case A1: Should return that the satellite is correctly oriented and does not need to be rotated.
 // prettier-ignore
-const correctlyOriented = new Int8Array([
-  // --- Upper half - sky should be here ---
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  // --- Lower half - Earth should be here ---
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-]);
+const correctlyOriented = new Int8Array(templates.correctlyOrientedTemplate);
 
 console.log(
+  "A1",
   calculateBalance(correctlyOriented, width, height, temperatureThreshold)
 );
 
-// Case 2: The satellite is correctly oriented - the upper half of the image is the sky, the lower half is the Earth.
-// The temperature of the sky and the Earth is uniform, but the temperature in the image varies by at most +/-10 C. The result should be still zero.
-// prettier-ignore
-const correctlyOrientedWithNoise = new Int8Array([
-  // --- Upper half ---
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  // --- Lower half ---
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-]);
-for (let i = 0; i < correctlyOrientedWithNoise.length; i++) {
-  const noise = Math.round(Math.random() * 20 - 10);
-  correctlyOrientedWithNoise[i] += noise;
-}
-
-console.log(
-  calculateBalance(
-    correctlyOrientedWithNoise,
-    width,
-    height,
-    temperatureThreshold
-  )
-);
-
-// Case 3: The satellite is slightly rotated in the axis perpendicular to the camera direction. The upper half of the image shows the sky AND Earth, and the lower half shows Earth.
+// Case A2: The satellite is slightly rotated in the axis perpendicular to the camera direction. The upper half of the image shows the sky AND Earth, and the lower half shows Earth.
 // Should return that the satellite is slightly rotated in the perpendicular axis, but not in the parallel axis.
 
 // prettier-ignore
-const tiltedPerpendicularAxis = new Int8Array([
-  // --- Upper half ---
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  // --- Lower half ---
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-]);
+const tiltedPerpendicularAxis = new Int8Array(templates.tiltedPerpendicularTemplate);
 
 console.log(
+  "A2",
   calculateBalance(tiltedPerpendicularAxis, width, height, temperatureThreshold)
 );
 
-// Case 4: The satellite is rotated in the axis parallel to the camera direction. The image is tilted by 45 degrees.
+// Case A3: The satellite is rotated in the axis parallel to the camera direction. The image is tilted by 45 degrees.
 // As the image is tilted clockwise, the satellite is tilted in the counter-clockwise direction from the camera's perspective.
 // Should return that the satellite is rotated in the parallel axis, but not in the perpendicular axis.
 
 // prettier-ignore
-const tiltedParallelAxis = new Int8Array([
-  // --- Upper half ---
-  10, -40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  10, 10, -40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  10, 10, 10, -40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  10, 10, 10, 10,  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  10, 10, 10, 10,  10, -40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  10, 10, 10, 10,  10, 10, -40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  // --- Lower half ---
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10,-40,-40, -40,-40,-40,-40,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10,-40, -40,-40,-40,-40,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10, -40,-40,-40,-40,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10,-40,-40,-40,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10,-40,-40,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10,-40
-]);
+const tiltedParallelAxis = new Int8Array(templates.tiltedParallelAxisTemplate);
 console.log(
+  "A3",
   calculateBalance(tiltedParallelAxis, width, height, temperatureThreshold)
 );
 
-// Case 5: The satellite is rotated in both axes. The image partly shows the sky in the upper right corner.
+// Case A4: The satellite is rotated in both axes. The image partly shows the sky in the upper right corner.
 // As the image is tilted clockwise, the satellite is tilted in the counter-clockwise direction from the camera's perspective.
 // prettier-ignore
-const tiltedBothAxes = new Int8Array([
-  // --- Upper half ---
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10,-40,-40, -40,-40,-40,-40,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10,-40, -40,-40,-40,-40,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10, -40,-40,-40,-40,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10,-40,-40,-40,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10,-40,-40,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10,-40,
-
-  // --- Lower half ---
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-]);
+const tiltedBothAxes = new Int8Array(templates.tiltedBothAxesTemplate);
 console.log(
+  "A4",
   calculateBalance(tiltedBothAxes, width, height, temperatureThreshold)
 );
 
-// Case 6: The satellite is rotated in the axis perpendicular to the camera direction. The upper half of the image shows the sky, and the lower half shows Earth and sky.
+// Case A5: The satellite is rotated in the axis perpendicular to the camera direction. The upper half of the image shows the sky, and the lower half shows Earth and sky.
 // Should return that the satellite is slightly rotated in the perpendicular axis, but not in the parallel axis, and should return negative value for the rotation in the perpendicular axis.
 // prettier-ignore
-const tiltedPerpendicularAxisReverse = new Int8Array([
-  // --- Upper half ---
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  // --- Lower half ---
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-  -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40, -40,-40,-40,-40,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,  10, 10, 10, 10,
-]);
+const tiltedPerpendicularAxisReverse = new Int8Array(templates.tiltedPerpendicularAxisReverseTemplate);
 console.log(
+  "A5",
   calculateBalance(
     tiltedPerpendicularAxisReverse,
     width,
@@ -197,15 +65,64 @@ console.log(
   )
 );
 
-// TODO:
-// Case 7: similar to case 5, but the sky is visible in the upper left corner instead.
-// Case 8: the Earth is visible only in the lower left corner, rest is sky.
-// Case 9: the Earth is visible only in the lower right corner, rest is sky.
-// Case 10: the image shows only the sky. In this case, the function should tell the satellite to keep rotating until the Earth is visible.
-// Case 11: the image shows only the Earth. In this case, the function should tell the satellite to keep rotating until the sky is visible, but in the opposite direction.
-// Case 12: the image shows the sky in the lower half and the Earth in the upper half. The satellite is rotated by 180 degrees in the perpendicular axis. The function should tell the satellite to keep rotating in the perpendicular axis.
+// Case A6: The image partly shows the sky in the upper left corner
+// Should return the same sign of imbalance in the perpendicular axis as in case A4, but opposite sign of imbalance in the parallel axis.
+const tiltedBothAxesReverse = new Int8Array(
+  templates.tiltedBothAxesReverseTemplate
+);
+console.log(
+  "A6",
+  calculateBalance(tiltedBothAxesReverse, width, height, temperatureThreshold)
+);
 
-// Next cases should be identical to the previous cases, but the image should include salt-and-pepper noise (random pixels with extreme values), simulating the effect of cosmic rays on the image sensor.
-// The function should return numbers close to the previous cases. The larger the noise, the difference should be larger.
+// Case A7: Earth is visible only in the lower right corner, rest is sky. The balance in perpendicular axis should be negative.
+const tiltedBothAxesMostlySky = new Int8Array(
+  templates.tiltedBothAxesMostlySkyTemplate
+);
+console.log(
+  "A7",
+  calculateBalance(tiltedBothAxesMostlySky, width, height, temperatureThreshold)
+);
+
+// Case B1: The satellite is correctly oriented - the upper half of the image is the sky, the lower half is the Earth.
+// The temperature of the sky and the Earth is uniform, but the temperature in the image varies by at most +/-10 C. The result should be still zero.
+// prettier-ignore
+const correctlyOrientedWithNoise = new Int8Array(templates.correctlyOrientedTemplate);
+inputModifiers.applyRandomNoise(correctlyOrientedWithNoise, 10);
+
+console.log(
+  "B1",
+  calculateBalance(
+    correctlyOrientedWithNoise,
+    width,
+    height,
+    temperatureThreshold
+  )
+);
+
+// Case C1: The satellite is correctly oriented - the upper half of the image is the sky, the lower half is the Earth.
+// The image from the camera is affected both by random noise and salt-and-pepper noise. The result should be close to zero.
+
+const correctlyOrientedWithTwoNoiseTypes = new Int8Array(
+  templates.correctlyOrientedTemplate
+);
+inputModifiers.applyRandomNoise(correctlyOrientedWithTwoNoiseTypes, 10);
+inputModifiers.applySaltAndPepperNoise(
+  correctlyOrientedWithTwoNoiseTypes,
+  0.01,
+  0.01
+);
+
+console.log(
+  "C1",
+  calculateBalance(
+    correctlyOrientedWithTwoNoiseTypes,
+    width,
+    height,
+    temperatureThreshold
+  )
+);
+
+// TODO: add more test cases to cover all the possible scenarios.
 
 // TODO: add a median or a morphological filter to the image to attempt to remove the noise and check how much it affects the result.
